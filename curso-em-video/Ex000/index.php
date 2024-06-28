c2499c513421d8df945265486861e38009b5ec8a
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-28 00:33:23 -0300

    hello world e phpinfo

---
+++ curso-em-video/Ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello World! \u{1F30E}";  
+        ?>
+    </h1>
+    <p>Primeiro Hello World em PHP</p>
+</body>
+</html>
