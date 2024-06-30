b4b71565d7cf40e59904bbaeb87d027e9e1c06f6
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-29 23:45:22 -0300

    aula variaveis e constantes

---
+++ curso-em-video/Ex003/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis</title>
+</head>
+<body>
+    <p>
+        <?php
+            $nome = "Bruno";
+            $sobrenome = "Fernandes";
+            const PAIS = "Brasil";
+            
+            echo "$nome $sobrenome você mora no " . PAIS;
+        ?>
+    </p>
+</body>
+</html>
