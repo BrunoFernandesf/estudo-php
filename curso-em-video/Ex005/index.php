0cd7be1c6251692c14a7a2fb14595c6e0d229aa4
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-07-01 19:17:14 -0300

    manipulando strings e formularios pt01

---
+++ curso-em-video/Ex005/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulando strings</title>
+</head>
+<body>
+    <?php 
+        // $nome = "Bruno";
+        // $sobrenome = "Fernandes";
+
+        // echo "O nome é: $nome";
+        // echo 'O sobrenome é: $nome';
+
+        $nom = "Rodrigo";
+        $snom = "Nogueira";
+        $apelido = '"Minotauro"';
+
+        echo "$nom $apelido $snom";
+    ?>
+</body>
+</html>
