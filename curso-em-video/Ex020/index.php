4bb6f00f54643f439ccc3ab387e2e31f2a242f01
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-07-04 18:55:46 -0300

    conversor de segundos

---
+++ curso-em-video/Ex020/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de Tempo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $segundos = $_GET['segundos'] ?? 0;
+
+    ?>
+    <main>
+        <h1>Calculadora de Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="segundos">Qual é o total de segundos?</label>
+            <input type="number" name="segundos" id="segundos" value="<?=$segundos?>">
+
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section>
+        <h2>Totalizando tudo</h2>
+        <?php 
+            //Cálculos
+            $minutos = intdiv($segundos, 60);
+            $horas = intdiv($minutos, 60);
+            $dias = intdiv($horas, 24);
+            $semanas = intdiv($dias, 7);
+       
+            echo "Analisando o valor que você digitou, <strong>$segundos segundos</strong> equivalem a um total de: ";
+            echo "<ul>"; 
+            echo "<li>$segundos segundos</li>";
+            echo "<li>$minutos minutos</li>";
+            echo "<li>$horas horas</li>";
+            echo "<li>$dias dias</li>";
+            echo "<li>$semanas semanas</li>";
+            echo "</ul>";
+        ?>
+    </section>
+</body>
+</html>
