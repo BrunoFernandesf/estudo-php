4c45fa5c8a507a5200de31b8becc7a354caffaad
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-07-08 20:58:53 -0300

    atualizando o css

--- curso-em-video/Ex008/index.php
+++ curso-em-video/Ex008/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sorteador de números</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <!--Gerar um número aleatório de 0 a 100
