6d11a90c15b0769490a78effc3b7f2544c010997
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-07-23 13:00:04 -0300

    atualizacao PDO

---
+++ php-udemy/pdo/exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+
+    $conn = new PDO("mysql:host=localhost;dbname=dbphp", "root", "***");
+
+    $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+
+    $login = "jose";
+    $password = "12444";
+
+    $stmt->bindParam(":LOGIN", $login);
+    $stmt->bindParam(":PASSWORD", $password);
+
+    $stmt->execute();
+
+?>
